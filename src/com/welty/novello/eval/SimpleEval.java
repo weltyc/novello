b109e0f80babaccbf6060f138c077c81f5ac2f59
Chris Welty <chris.welty.1@gmail.com>
2014-01-28 10:01:57 -0600

    add Ivan as a player

--- src/com/welty/novello/eval/SimpleEval.java
+++ src/com/welty/novello/eval/SimpleEval.java
@@ -23,75 +23,120 @@ public abstract class SimpleEval extends Eval {
         return evalFromName.get(name);
     }
 
+    public static Collection<String> getEvalNames() {
+        return evalFromName.keySet();
+    }
+
     SimpleEval(String name) {
         this.name = name;
         evalFromName.put(name, this);
     }
 
     @Override public int eval(long mover, long enemy) {
-        final long moverMoves = BitBoardUtils.calcMoves(mover, enemy);
-        final long enemyMoves = BitBoardUtils.calcMoves(enemy, mover);
-        return random() + eval(mover, enemy, moverMoves, enemyMoves);
+        return random.nextInt(2 * DISK_VALUE) + eval(new Situation(mover, enemy));
     }
-
-    protected abstract int eval(long mover, long enemy, long moverMoves, long enemyMoves);
-
-    private static final int[] coeffs = {0, 800, -800, 500, 1000, -1000, -600, 600};
-
-    private static int corner2Value(long mover, long enemy, long moverMoves, long enemyMoves) {
-        int eval = 0;
-        for (CornerTerm2 term : CornerTerm2.terms) {
-            final int instance = term.instance(mover, enemy, moverMoves, enemyMoves);
-            eval += coeffs[instance];
-        }
-        return eval;
-    }
-
-    private static int random() {
-        return random.nextInt(2 * DISK_VALUE);
-    }
-
-    private static int netDisks(long mover, long enemy) {
-        return Long.bitCount(mover) - Long.bitCount(enemy);
-    }
-
-    public static Eval Greedy = new SimpleEval("Abigail") {
-        @Override public int eval(long mover, long enemy, long moverMoves, long enemyMoves) {
-            return DISK_VALUE * netDisks(mover, enemy);
-        }
-    };
-
-    public static Eval Corny = new SimpleEval("Charlie") {
-        @Override public int eval(long mover, long enemy, long moverMoves, long enemyMoves) {
-            final int netCorners = netDisks(mover & BitBoardUtils.CORNERS, enemy & BitBoardUtils.CORNERS);
-            return DISK_VALUE * (netDisks(mover, enemy) + 9 * netCorners);
-        }
-    };
-
-    public static Eval Nox = new SimpleEval("Ethelred") {
-
-        @Override public int eval(long mover, long enemy, long moverMoves, long enemyMoves) {
-            int eval = corner2Value(mover, enemy, moverMoves, enemyMoves);
-            return eval + netDisks(mover, enemy) * DISK_VALUE;
-        }
-    };
-
-    public static Eval Mob = new SimpleEval("Gertrude") {
-        @Override public int eval(long mover, long enemy, long moverMoves, long enemyMoves) {
-            int eval = DISK_VALUE*(netDisks(moverMoves, enemyMoves)) + corner2Value(mover, enemy, moverMoves, enemyMoves);
-            final int nEmpty = BitBoardUtils.nEmpty(mover, enemy);
-            if (nEmpty < 8) {
-                eval = ((8-nEmpty)*netDisks(mover, enemy) + nEmpty*eval)/ 8;
-            }
-            return eval;
-        }
-    };
 
     @Override public String toString() {
         return name;
     }
 
-    public static Collection<String> getEvalNames() {
-        return evalFromName.keySet();
+    protected abstract int eval(Situation s);
+
+    // Initialize inbuilt players
+    static {
+        new SimpleEval("Abigail") {
+            @Override public int eval(Situation s) {
+                return s.netDisks();
+            }
+        };
+
+        new SimpleEval("Charlie") {
+            @Override public int eval(Situation s) {
+                return s.netDisks() + 9 * s.netCorners();
+            }
+        };
+
+        new SimpleEval("Ethelred") {
+            @Override public int eval(Situation s) {
+                return s.netDisks() + s.corner2Value();
+            }
+        };
+
+        new SimpleEval("Gertrude") {
+            @Override public int eval(Situation s) {
+                return s.interpolate(s.netMobs() + s.corner2Value());
+            }
+        };
+
+        new SimpleEval("Ivan") {
+            @Override public int eval(Situation s) {
+                return s.interpolate(s.netMobs() + s.corner2Value() + s.netPotMobs() / 2);
+            }
+        };
+    }
+
+    private static final int[] coeffs = {0, 800, -800, 500, 1000, -1000, -600, 600};
+
+    /**
+     * board situation + evaluation components for simple players
+     */
+    private static class Situation {
+        private final long mover;
+        private final long enemy;
+        private final long moverMoves;
+        private final long enemyMoves;
+
+        Situation(long mover, long enemy) {
+            this.mover = mover;
+            this.enemy = enemy;
+            this.moverMoves = BitBoardUtils.calcMoves(mover, enemy);
+            this.enemyMoves = BitBoardUtils.calcMoves(enemy, mover);
+        }
+
+        private static int netCentidisks(long a, long b) {
+            return DISK_VALUE * (Long.bitCount(a) - Long.bitCount(b));
+        }
+
+        private int interpolate(int eval) {
+            final int nEmpty = nEmpty();
+            if (nEmpty < 8) {
+                eval = ((8 - nEmpty) * netDisks() + nEmpty * eval) / 8;
+            }
+            return eval;
+        }
+
+        private int netDisks() {
+            return netCentidisks(mover, enemy);
+        }
+
+        private int netMobs() {
+            return netCentidisks(moverMoves, enemyMoves);
+        }
+
+        private int netPotMobs() {
+            final long empty = empty();
+            return netCentidisks(BitBoardUtils.potMobs(enemy, empty), BitBoardUtils.potMobs(mover, empty));
+        }
+
+        private long empty() {
+            return ~(mover | enemy);
+        }
+
+        private int netCorners() {
+            return netCentidisks(mover & BitBoardUtils.CORNERS, enemy & BitBoardUtils.CORNERS);
+        }
+
+        private int corner2Value() {
+            int eval = 0;
+            for (CornerTerm2 term : CornerTerm2.terms) {
+                final int instance = term.instance(mover, enemy, moverMoves, enemyMoves);
+                eval += coeffs[instance];
+            }
+            return eval;
+        }
+
+        public int nEmpty() {
+            return Long.bitCount(empty());
+        }
     }
 }
