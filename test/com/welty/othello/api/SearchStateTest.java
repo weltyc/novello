88e84bec36c4a890319ea09fcf41f31b1e55e432
Chris Welty <chris.welty.1@gmail.com>
2014-02-12 16:45:22 -0600

    Make OsClock immutable; change position clocks array to blackClock and whiteClock; update clocks when a move is added

--- test/com/welty/othello/api/SearchStateTest.java
+++ test/com/welty/othello/api/SearchStateTest.java
@@ -1,12 +1,13 @@
 package com.welty.othello.api;
 
 import com.welty.othello.gdk.COsGame;
+import com.welty.othello.gdk.OsClock;
 import junit.framework.TestCase;
 
 public class SearchStateTest extends TestCase {
     public void testConstructor() {
         final COsGame game = new COsGame();
-        game.Initialize("8");
+        game.Initialize("8", OsClock.DEFAULT, OsClock.DEFAULT);
 
         final SearchState searchState = new SearchState(game, 1, 0);
         assertEquals(0, searchState.getGame().getMoveList().size());
